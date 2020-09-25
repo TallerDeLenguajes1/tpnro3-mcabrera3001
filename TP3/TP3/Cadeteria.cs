67f3e921ea66160aea884365302dc8120db95022
Your Name <you@example.com>
2020-09-25 16:57:12 -0300

    Avance TP3

---
+++ TP3/TP3/Cadeteria.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace TP3
+{
+    
+
+    enum Estado
+    {
+        enviado,
+        entregado,
+    };
+    class Cadeteria
+    {
+        string nombre;
+        List<Cadete> cadetes;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public List<Cadete> Cadetes { get => cadetes; set => cadetes = value; }
+
+        public Cadeteria(string Nombre)
+        {
+            this.Nombre = Nombre;
+            Cadetes = new List<Cadete>();
+        }
+
+        public void agregarCadetes(Cadete nuevoCadete)
+        {
+            Cadetes.Add(nuevoCadete);
+        }
+    }
+
+    class Pedido
+    {
+        int nroPedido;
+        string observaciones;
+        Estado estadoPedido;
+
+        public int NroPedido { get => nroPedido; set => nroPedido = value; }
+        public string Observaciones { get => observaciones; set => observaciones = value; }
+        public Estado EstadoPedido { get => estadoPedido; set => estadoPedido = value; }
+
+        Random rand = new Random();
+        public Pedido(int nro, int idCliente, string nombreCliente, string direccionCliente, int celularCliente)
+        {
+            nroPedido = nro;
+            estadoPedido = Estado.enviado;
+            Cliente nuevoCliente = new Cliente(idCliente, nombreCliente, direccionCliente, celularCliente);
+        }
+    }
+
+    class Cliente
+    {
+        int idCliente;
+        string nombreCliente;
+        string direccionCliente;
+        int celularCliente;
+
+        public int IdCliente { get => idCliente; set => idCliente = value; }
+        public string NombreCliente { get => nombreCliente; set => nombreCliente = value; }
+        public string DireccionCliente { get => DireccionCliente; set => DireccionCliente = value; }
+        public int CelularCliente { get => celularCliente; set => celularCliente = value; }
+
+        public Cliente(int idCliente, string nombreCliente, string direccionCliente, int celularCliente)
+        {
+            this.celularCliente = idCliente;
+            this.nombreCliente = nombreCliente;
+            this.direccionCliente = direccionCliente;
+            this.celularCliente = celularCliente;
+        }
+    }
+    class Cadete
+    {
+        int idCadete;
+        string nombreCadete;
+        string direccionCadete;
+        int celularCadete;
+        List<Pedido> pedidos;
+ 
+
+        public int IdCadete { get => idCadete; set => idCadete = value; }
+        public string NombreCadete { get => nombreCadete; set => nombreCadete = value; }
+        public string DireccionCadete { get => direccionCadete; set => direccionCadete = value; }
+        public int CelularCadete { get => celularCadete; set => celularCadete = value; }
+        internal List<Pedido> Pedidos { get => pedidos; set => pedidos = value; }
+
+        public Cadete(int idCadete, string nombreCadete , string direccionCadete, int CelularCadete)
+        {
+            this.IdCadete = idCadete;
+            this.NombreCadete = nombreCadete;
+            this.DireccionCadete = direccionCadete;
+            this.celularCadete = CelularCadete;
+
+        }
+
+        public void agregarPedidos(Pedido nuevoPedido)
+        {
+            Pedidos.Add(nuevoPedido);
+        }
+
+
+    }
+}
---
+++ TP3/TP3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nombres = { "Pedro", "Juan", "Maria", "Romina", "Horacio", "Oscar", "Walter", "Rodrigo" };
+            string[] direcciones = { "BarrioNorte", "ZonaSur", "Manatial", "LaBanda", "Alderetes" };
+
+            Cadeteria RappiMensajes = new Cadeteria("RappiMensajes");
+            Random rand = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Cadete nuevoCadete = new Cadete(i, nombres[rand.Next(7)], direcciones[rand.Next(4)], rand.Next(155000000, 157000000));
+                RappiMensajes.Cadetes.Add(nuevoCadete);
+            }
+            
+
+            for (int i = 0; i < 10; i++)
+            {
+                
+                
+                //creo el pedido junto con el cliente y lo agrego a cada lista de cada cadete
+                Pedido nuevoPedido = new Pedido(rand.Next(100), rand.Next(100), nombres[rand.Next(7)], direcciones[rand.Next(4)], rand.Next(rand.Next(155000000, 157000000)));
+                nuevoCadete.agregarPedidos(nuevoPedido);
+
+            }
+
+
+      
+            
+            
+        }
+    }
+}
